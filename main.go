1e562a204cf1734c24d1699d404c3f5df2afea82
David Alexander <opensource@thelonelyghost.com>
2025-08-19 17:32:07 -0400

    Exit non-zero on error

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/thelonelyghost/vault-aws-credential-protocol/cmd"
 )
 
 func main() {
 	ctx := context.Background()
-	cmd.Execute(ctx)
+	if err := cmd.Execute(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
